9634dac934a0cf06ecddb1d9d83657d917ff255b
Brian Litzinger <litzinger@gmail.com>
2025-10-01 09:34:23 -0500

    Add IndexUserCommand

---
+++ src/Service/Indexer/IndexUserCommand.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Litzinger\DexterCore\Service\Indexer;
+
+use League\Pipeline\PipelineBuilder;
+use Litzinger\DexterCore\Contracts\ConfigInterface;
+use Litzinger\DexterCore\Contracts\IndexableInterface;
+
+class IndexUserCommand implements IndexCommand
+{
+    public function __construct(
+        public string $indexName,
+        public IndexableInterface $indexable,
+        public ConfigInterface $config,
+        public array $pipelines,
+        public string $queueJobName,
+    ) {
+    }
+
+    public function execute(): array
+    {
+        $pipelineBuilder = new PipelineBuilder;
+
+        foreach ($this->pipelines as $pipelineClass) {
+            $pipelineBuilder->add(
+                new $pipelineClass(
+                    $this->indexable,
+                    $this->config,
+                )
+            );
+        }
+
+        $pipelines = $pipelineBuilder->build();
+
+        return $pipelines->process($this->getValues());
+    }
+
+    public function getIndexName(): string
+    {
+        return $this->indexName;
+    }
+
+    public function getValues(): array
+    {
+        return $this->indexable->getValues();
+    }
+
+    public function getId(): int
+    {
+        return $this->indexable->getId();
+    }
+
+    public function getUniqueId(): string
+    {
+        // EE is member_, Craft is user_
+        $prefix = defined('APP_VER') ? 'member_' : 'user_';
+
+        return $prefix . $this->indexable->getId();
+    }
+
+    public function getQueueJobName(): string
+    {
+        return $this->queueJobName;
+    }
+}
